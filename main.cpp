bab7232aca40008d15ac5f3b95d7370282d20f37
dungcatcher <ewan.odenthal1@gmail.com>
2022-05-15 16:03:51 +1000

    Now includes header files instead of cpp files

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
-#include "board.cpp"
-#include "movegen.cpp"
+#include "board.hpp"
+#include "movegen.hpp"
 
 int main()
 {
     Board board;
     std::cout << board.prettyPrint();
     generateWPawnMoves(board.getWhitePawns(), ~board.getOccupied());
+    
     return 0;
 }
--- movegen.hpp
+++ movegen.hpp
@@ -22,10 +22,31 @@ uint64_t westOne (uint64_t b);
 uint64_t soWeOne (uint64_t b);
 uint64_t noWeOne (uint64_t b);
 
+const int index64[64] = {
+    0, 47,  1, 56, 48, 27,  2, 60,
+   57, 49, 41, 37, 28, 16,  3, 61,
+   54, 58, 35, 52, 50, 42, 21, 44,
+   38, 32, 29, 23, 17, 11,  4, 62,
+   46, 55, 26, 59, 40, 36, 15, 53,
+   34, 51, 20, 43, 31, 22, 10, 45,
+   25, 39, 14, 33, 19, 30,  9, 24,
+   13, 18,  8, 12,  7,  6,  5, 63
+};
+
+/**
+ * bitScanForward
+ * @author Kim Walisch (2012)
+ * @param bb bitboard to scan
+ * @precondition bb != 0
+ * @return index (0..63) of least significant one bit
+ */
+int bitScanForward(uint64_t bb);
+
 uint64_t wSinglePushTargets(uint64_t wpawns, uint64_t empty);
 uint64_t wDblPushTargets(uint64_t wpawns, uint64_t empty);
 uint64_t bSinglePushTargets(uint64_t bpawns, uint64_t empty);
 uint64_t bDoublePushTargets(uint64_t bpawns, uint64_t empty);
 uint64_t wPawnsAble2Push(uint64_t wpawns, uint64_t empty);
 uint64_t wPawnsAble2DblPush(uint64_t wpawns, uint64_t empty);
-void generatePawnMoves(uint64_t wpawns, uint64_t empty);
+void generateWPawnMoves(uint64_t wpawns, uint64_t empty);
+void generateBPawnMoves(uint64_t bpawns, uint64_t empty);
