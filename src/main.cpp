fd3b390b6e37f6c68691f4c76c1c6d8cbcf24997
dungcatcher <ewan.odenthal1@gmail.com>
2022-08-02 20:29:08 +1000

    Fixed multiple bugs with movegen

--- src/main.cpp
+++ src/main.cpp
@@ -10,36 +10,14 @@
 
 int main()
 {
-    std::string startFen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R";
+    std::string startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     Board board(startFen);
-
-    board.turn = 1;
-
-    Move move;
-    move.start = 4;
-    move.end = 2;
-    move.flags = 0x3;
-    board.makeMove(move);
-
-    Move move2;
-    move.start = 60;
-    move.end = 58;
-    move.flags = 0x3;
-    board.makeMove(move2);
 
     initRays();
     initTables();
-
-    std::vector<Move> moveList;
-    moveList = generateLegalMoves(moveList, board);
-    for (auto &move : moveList) {
-        Board newBoard = board;
-        newBoard.makeMove(move);
-        std::cout << coordinateIndexTable[move.start] << coordinateIndexTable[move.end] << ": " << perft(0, newBoard) << "\n";
-    }
     
     auto start = std::chrono::steady_clock::now();
-    int perftNum = perft(1, board);
+    int perftNum = perft(6, board, true);
     std::cout << perftNum << "\n";
     auto end = std::chrono::steady_clock::now();
 
