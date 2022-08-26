9e73b451ec5229f42ec59ddfa9400d9f92094842
dungcatcher <ewan.odenthal1@gmail.com>
2022-08-26 15:19:25 +1000

    Removed redundant turn argument from eval

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,13 +1,13 @@
 #include "eval.hpp"
 
-int evaluate(const Board& board, bool turn) {
+int evaluate(const Board& board) {
     int score = 0;
 
-    score += PAWN_VALUE * (__builtin_popcountll(board.getWhitePawns()) - __builtin_popcountll(board.getBlackPawns()));
+    score += PAWN_VALUE   * (__builtin_popcountll(board.getWhitePawns()  ) - __builtin_popcountll(board.getBlackPawns())  );
     score += KNIGHT_VALUE * (__builtin_popcountll(board.getWhiteKnights()) - __builtin_popcountll(board.getBlackKnights()));
     score += BISHOP_VALUE * (__builtin_popcountll(board.getWhiteBishops()) - __builtin_popcountll(board.getBlackBishops()));
-    score += ROOK_VALUE * (__builtin_popcountll(board.getWhiteRooks()) - __builtin_popcountll(board.getBlackRooks()));
-    score += QUEEN_VALUE * (__builtin_popcountll(board.getWhiteQueens()) - __builtin_popcountll(board.getBlackQueens()));
+    score += ROOK_VALUE   * (__builtin_popcountll(board.getWhiteRooks()  ) - __builtin_popcountll(board.getBlackRooks())  );
+    score += QUEEN_VALUE  * (__builtin_popcountll(board.getWhiteQueens() ) - __builtin_popcountll(board.getBlackQueens()) );
     
     return score;
 }
